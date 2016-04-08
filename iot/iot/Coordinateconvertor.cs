da5df639fc3b120bbd77a1808225c299eaaba043
ajatgd <akshayvashistha1995@gmail.com>
2016-04-09 01:02:49 +0530

    initial commit

---
+++ iot/iot/Coordinateconvertor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iot
+{
+    class Coordinateconvertor
+    {
+        public static string Longitude = "akshay";
+        public static string Latitude = null;
+
+        public async static void getlocation()
+        {
+            Geolocator geolocator = new Geolocator();
+            geolocator.DesiredAccuracyInMeters = 50;
+
+            try
+            {
+                Geoposition geoposition = await geolocator.GetGeopositionAsync(
+                    maximumAge: TimeSpan.FromMinutes(5),
+                    timeout: TimeSpan.FromSeconds(10)
+                    );
+                Longitude = geoposition.Coordinate.Point.Position.Longitude.ToString("0.00000");
+                Latitude = geoposition.Coordinate.Point.Position.Latitude.ToString("0.00000");
+
+
+            }
+            catch (Exception ex)
+            {
+                Latitude = "please go back and wait for 5-10sec";
+                Longitude = "please go back and wait for 5-10sec";
+                if ((uint)ex.HResult == 0x80004004)
+                {
+                    Latitude = "please go back and wait for 5-10sec";
+                    Longitude = "please go back and wait for 5-10sec";
+                }
+            }
+            {
+
+            }
+        }
+    }
+}
---
+++ iot/iot/MainPage.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace iot
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "http://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=181cacb355f29ff0dd2fd86b12871818";
+
+            HttpClient client = new HttpClient();
+            string stringdate = await client.GetStringAsync(new Uri(url));
+
+
+            RootObject data = JsonConvert.DeserializeObject<RootObject>(stringdate);
+
+            //result1.Text = data.main.temp_max.ToString();
+            //result2.Text = data.main.temp_min.ToString();
+            //result3.Text = data.weather[0].main;
+        }
+    }
+    public class Coord
+    {
+        public double lon { get; set; }
+        public double lat { get; set; }
+    }
+
+    public class Sys
+    {
+        public int population { get; set; }
+    }
+
+    public class City
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public Coord coord { get; set; }
+        public string country { get; set; }
+        public int population { get; set; }
+        public Sys sys { get; set; }
+    }
+
+    public class Main
+    {
+        public double temp { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+        public double pressure { get; set; }
+        public double sea_level { get; set; }
+        public double grnd_level { get; set; }
+        public int humidity { get; set; }
+        public int temp_kf { get; set; }
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    public class Clouds
+    {
+        public int all { get; set; }
+    }
+
+    public class Wind
+    {
+        public double speed { get; set; }
+        public double deg { get; set; }
+    }
+
+    public class Rain
+    {
+        public double __invalid_name__3h { get; set; }
+    }
+
+    public class Sys2
+    {
+        public string pod { get; set; }
+    }
+
+    public class Snow
+    {
+        public double __invalid_name__3h { get; set; }
+    }
+
+    public class List
+    {
+        public int dt { get; set; }
+        public Main main { get; set; }
+        public List<Weather> weather { get; set; }
+        public Clouds clouds { get; set; }
+        public Wind wind { get; set; }
+        public Rain rain { get; set; }
+        public Sys2 sys { get; set; }
+        public string dt_txt { get; set; }
+        public Snow snow { get; set; }
+    }
+
+    public class RootObject
+    {
+        public City city { get; set; }
+        public string cod { get; set; }
+        public double message { get; set; }
+        public int cnt { get; set; }
+        public List<List> list { get; set; }
+    }
+}
